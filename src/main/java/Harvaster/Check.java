f0819d4619556229376a8b555e55d5415608662f
Biniobiniasty <Biniobiniasty@gmail.com>
2022-07-27 06:05:06 -0700

    add check

--- src/main/java/Harvaster/Check.java
+++ src/main/java/Harvaster/Check.java
@@ -8,14 +8,13 @@ public class Check {
 
 	// Here is checked data
 	private static volatile Queue<Check> DataToSeg = new ArrayDeque<>();
-	
-	
+
 	private HashMap<String, String> data;
 	private boolean vaildData;
 	private int counterConfirmation;
 
-	private synchronized static void addData(Check newData) { 
-		if(newData.vaildData)
+	private synchronized static void addData(Check newData) {
+		if (newData.vaildData)
 			DataToSeg.add(newData);
 	}
 
@@ -24,7 +23,7 @@ public class Check {
 			return DataToSeg.remove();
 		return null;
 	}
-	
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	public Check(String name) {
 		vaildData = false;
@@ -33,144 +32,167 @@ public class Check {
 		data.put("name", name);
 		checkName(name);
 	}
-	
-	public boolean checkAndPut()
-	{
-		if(counterConfirmation == 7) // every function checked have to add to one
-		{	
+
+	public boolean checkAndPut() {
+		if (counterConfirmation == 7) // every function checked have to add to one
+		{
 			vaildData = true;
 			addData(this);
 			return true;
 		}
 		return false;
 	}
-	
-	public void addData(String name, String value)
-	{
+
+	public void addData(String name, String value) {
 		data.put(name, value);
 	}
-	
+
 ////////////////////////////////////////SETTERS/////////////////////////////////////
-	
-	public boolean setETHvalue(String value)
-	{
-		if(checkETHvalue(value))
-		{
+
+	public boolean setETHvalue(String value) {
+		if (checkETHvalue(value)) {
 			addData("ETH-value", value);
 			return true;
 		}
 		return false;
 	}
-	
-	public boolean setBTCvolumen(String value)
-	{
-		if(checkBTCvolumen(value))
-		{
+
+	public boolean setBTCvolumen(String value) {
+		if (checkBTCvolumen(value)) {
 			addData("BTC-volumen", value);
 			return true;
 		}
 		return false;
 	}
-	
-	public boolean setBTCvalue(String value)
-	{
-		if(checkBTCvalue(value))
-		{
+
+	public boolean setBTCvalue(String value) {
+		if (checkBTCvalue(value)) {
 			addData("BTC-value", value);
-			return true; 
+			return true;
 		}
 		return false;
 	}
-	
-	public boolean setETHvolumen(String value)
-	{
-		if(checkETHvolumen(value))
-		{
+
+	public boolean setETHvolumen(String value) {
+		if (checkETHvolumen(value)) {
 			addData("ETH-volumen", value);
 			return true;
 		}
 		return false;
 	}
 
-
 	public boolean setSellBTC(String value) {
-		if(checkSellBTC(value))
-		{
+		if (checkSellBTC(value)) {
 			addData("Sell-BTC", value);
 			return true;
 		}
 		return false;
 	}
-	
+
 	public boolean setBuyBTC(String value) {
-		if(checkBuyBTC(value))
-		{
+		if (checkBuyBTC(value)) {
 			addData("Buy-BTC", value);
 			return true;
 		}
 		return false;
 	}
-	
+
 ////////////////////////////////////////GETTERS/////////////////////////////////////
-	public double getETHvalue()
-	{
+	public double getETHvalue() {
 		return Double.parseDouble(data.get("ETH-value"));
 	}
-	public double getBTCvalue()
-	{
+
+	public double getBTCvalue() {
 		return Double.parseDouble(data.get("BTC-value"));
 	}
+
 	public String getName() {
 		return data.get("name");
 	}
+
 	public double getSellBTC() {
 		return Double.parseDouble(data.get("Sell-BTC"));
 	}
+
 	public double getBuyBTC() {
 		return Double.parseDouble(data.get("Buy-BTC"));
-	}	
+	}
+
 	public double getETHvolumen() {
 		return Double.parseDouble(data.get("ETH-volumen"));
 	}
-	
+
 	public double getBTCvolumen() {
 		return Double.parseDouble(data.get("BTC-volumen"));
 	}
-	
+
 ///////////////////////////////////////CHECK DATA////////////////////////////////////
-	private boolean checkETHvalue(String value)
-	{
+	private boolean checkETHvalue(String value) {
 		counterConfirmation++;
 		return true;
 	}
+
 	private boolean checkName(String name) {
-		counterConfirmation++;
-		return true;
+		if (name.length() > 0) {
+			counterConfirmation++;
+			return true;
+		}
+		return false;
 	}
 
 	private boolean checkSellBTC(String sell) {
-		counterConfirmation++;
-		return true;
+		if (checkDigit(sell)) {
+			counterConfirmation++;
+			return true;
+		}
+		return false;
 	}
 
 	private boolean checkBuyBTC(String buy) {
-		counterConfirmation++;
-		return true;
-	}
-	
+		if (checkDigit(buy)) {
+			counterConfirmation++;
+			return true;
+		}
+		return false;	}
+
 	private boolean checkETHvolumen(String value) {
-		counterConfirmation++;
-		return true;
-	}
-	
+		if (checkDigit(value)) {
+			counterConfirmation++;
+			return true;
+		}
+		return false;}
+
 	private boolean checkBTCvalue(String buy) {
-		counterConfirmation++;
-		return true;
-	}
-	
+		if (checkDigit(buy)) {
+			counterConfirmation++;
+			return true;
+		}
+		return false;}
+
 	private boolean checkBTCvolumen(String value) {
-		counterConfirmation++;
-		return true;
+		if (checkDigit(value)) {
+			counterConfirmation++;
+			return true;
+		}
+		return false;}
+
+	private boolean checkDigit(String value) {
+		int counter = 0;
+		boolean result = false;
+
+		for (int x = 0; x < value.length(); x++) {
+			char znak = value.toCharArray()[x];
+			if (znak == '0' || znak == '1' || znak == '2' || znak == '3' || znak == '4' || znak == '5' || znak == '6'
+					|| znak == '7' || znak == '8' || znak == '9' || znak == '.') {
+				counter++;
+				continue;
+			}
+		}
+
+		if (counter == value.length())
+			result = true;
+
+		return result;
 	}
 
 }
--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -47,6 +47,7 @@ public class Main {
 	 */
 	public Main() {
 		initialize();
+		frmAnalizer.setResizable(false);
 	}
 
 	/**
